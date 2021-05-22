50f8e9124248d2c373e3f3c14c141e76248aea13
Дмитров Артем <odolisk@yandex.ru>
2021-05-22 11:39:55 +0300

    Fix wsgi

--- memorium/memorium/wsgi.py
+++ memorium/memorium/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memorium.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memorium.memoruim.settings')
 
 application = get_wsgi_application()
